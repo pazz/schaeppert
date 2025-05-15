82f396711f367e881ab738eac015665c9f6556d9
Patrick Totzke <patrick@totzke.uk>
2025-05-15 11:03:25 +0100

    iterative procedure v1

--- src/iterative.rs
+++ src/iterative.rs
@@ -1,11 +1,16 @@
 use clap::Parser;
+use log::{debug, info};
 use std::path::PathBuf;
-use log::info;
 
 use shepherd::nfa;
 mod logging;
+use regex::Regex;
+use std::fs::File;
+use std::io;
+use std::io::{Read, Write};
+use std::process::{Command, Stdio};
 
-
+const PRISM_CMD: &str = "prism";
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,10 +18,11 @@ pub struct Args {
     #[arg(value_name = "AUTOMATON_FILE", help = "Path to the input")]
     pub filename: String,
 
-    #[arg(value_name = "N",
-           help = "dimension of the product"
-       )]
-    pub dim: usize,
+    #[arg(
+        value_name = "TMP_DIR",
+        help = "directory where to store the prism files"
+    )]
+    pub tmp_dir: PathBuf,
 
     #[arg(
         short = 'f',
@@ -42,7 +48,6 @@ pub struct Args {
         help = "Optional path to the log file. Defaults to stdout if not specified."
     )]
     pub log_output: Option<PathBuf>,
-
 }
 
 pub fn main() {
@@ -53,7 +58,11 @@ pub fn main() {
     logging::setup_logger(args.verbosity, args.log_output);
 
     // parse the input file
-    let mut nfa = nfa::Nfa::load_from_file(&args.filename, &args.input_format, &nfa::StateOrdering::Alphabetical);
+    let mut nfa = nfa::Nfa::load_from_file(
+        &args.filename,
+        &args.input_format,
+        &nfa::StateOrdering::Alphabetical,
+    );
 
     // print the input automaton
     info!("{}", nfa);
@@ -66,20 +75,82 @@ pub fn main() {
             Ok(sink) => {
                 info!("Added sink state");
                 nfa.complete(Some(sink));
-            },
+            }
             Err(e) => {
                 info!("Error adding sink state: {}", e);
-                return;  // TODO:  handle this error properly
+                return; // TODO:  handle this error properly
             }
         }
     }
     // print the complete automaton again
     info!("{}", nfa);
 
-    // TODO: create prism input file
-    let prism_model = nfa_to_prism(&nfa, args.dim);
-    print!("{}", prism_model);
-    // TODO: call prism
+    let mut i = 1;
+    loop {
+        // create prism input string
+        let prism_model = nfa_to_prism(&nfa, i);
+        debug!("{}", prism_model);
+
+        // write prism input to file
+        let prism_input_path = args.tmp_dir.join(format!("model-{}.pm", i));
+        if let Err(e) = write_string_to_file(&prism_model, &prism_input_path) {
+            eprintln!("Error writing to file: {}", e);
+            return;
+        }
+        info!("Wrote prism input to file: {}", prism_input_path.display());
+
+        let value = call_prism(&[
+            "-pf",
+            "Pmax=? [ F \"final\" ]",
+            &prism_input_path.to_string_lossy(),
+        ])
+        .unwrap();
+        println!("n={} -> {:.3}", i, value);
+
+        if value < 1.0 {
+            println!("The value is less than 1.0, stopping the search.");
+            break;
+        }
+        i += 1;
+    }
+}
+
+fn write_string_to_file(content: &str, file_path: &PathBuf) -> io::Result<()> {
+    let mut file = File::create(file_path)?;
+    file.write_all(content.as_bytes())?;
+    Ok(())
+}
+
+fn call_prism(args: &[&str]) -> Result<f32, ()> {
+    let child = Command::new(PRISM_CMD)
+        .args(args)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to call prism");
+
+    let stdout = child.stdout.expect("Failed to capture stdout");
+
+    // Using stdout to read the output from the child process
+    let mut output = String::new();
+    io::BufReader::new(stdout)
+        .read_to_string(&mut output)
+        .expect("Failed to read from stdout");
+    info!(
+        "PRISM OUTPUT\n---------------\n{}\n-----------------",
+        output
+    );
+
+    // Compile the regular expression once
+    let re = Regex::new(r"Value in the initial state: (\d+\.\d+)").unwrap();
+
+    for line in output.lines() {
+        if let Some(captures) = re.captures(&line) {
+            if let Some(value) = captures.get(1) {
+                return value.as_str().parse::<f32>().map_err(|_| ());
+            }
+        }
+    }
+    Err(())
 }
 
 fn nfa_to_prism(nfa: &nfa::Nfa, n: usize) -> String {
@@ -90,33 +161,37 @@ fn nfa_to_prism(nfa: &nfa::Nfa, n: usize) -> String {
     prism_input.push_str("module M1\n");
 
     // we assume that there is only one initial state; get it.
-    let initial :nfa::State = nfa.initial_states().iter().cloned().next().unwrap();
+    let initial: nfa::State = nfa.initial_states().iter().cloned().next().unwrap();
 
     // define states string for prism
-    prism_input.push_str(&format!("s1 : [0..{}] init {initial};\n", nfa.nb_states()-1));
+    prism_input.push_str(&format!(
+        "s1 : [0..{}] init {initial};\n",
+        nfa.nb_states() - 1
+    ));
 
     // define transitions
-    for (act,am) in nfa.get_edges().iter() {    // for every alphabet letter
-        for src in 0..am.dim()  {             // for all states
+    for (act, am) in nfa.get_edges().iter() {
+        // for every alphabet letter
+        for src in 0..am.dim() {
+            // for all states
             let succs = am.get_successors(src); // get successors
-            // prism requires explicit floating point numbers to represent distributions.
-            // here we represent a uniform dist among successors.
-            let prob = 1.0/succs.len() as f64;
-            let update = succs.iter()
-                            .map(|trg| format!("{}:(s1'={})", prob, trg))
-                            .collect::<Vec<String>>()
-                            .join(" + ");
+                                                // prism requires explicit floating point numbers to represent distributions.
+                                                // here we represent a uniform dist among successors.
+            let prob = 1.0 / succs.len() as f64;
+            let update = succs
+                .iter()
+                .map(|trg| format!("{}:(s1'={})", prob, trg))
+                .collect::<Vec<String>>()
+                .join(" + ");
             prism_input.push_str(&format!("[{act}] s1={} -> {};\n", src, update));
         }
     }
     prism_input.push_str("endmodule\n\n");
 
-
     // Add a copy of the MDP for every power up to n
     for i in 2..=n {
         prism_input.push_str(&format!("module M{i} = M1 [s1=s{i}, s{i}=s1] endmodule\n"));
     }
-
 
     // define a label representing global reachability target:
     // every component is in one of its final states.
@@ -124,26 +199,31 @@ fn nfa_to_prism(nfa: &nfa::Nfa, n: usize) -> String {
 
     let mut conj = Vec::new();
     for i in 1..=n {
-        conj.push(nfa.final_states().iter()
-            .map(|f| format!("s{i}={f}"))
-            .collect::<Vec<_>>()
-            .join("| "));
+        conj.push(
+            nfa.final_states()
+                .iter()
+                .map(|f| format!("s{i}={f}"))
+                .collect::<Vec<_>>()
+                .join("| "),
+        );
     }
-    final_line.push_str(&conj.iter()
-        .map(|f| format!("( {f} )"))
-        .collect::<Vec<_>>()
-        .join(" & "));
+    final_line.push_str(
+        &conj
+            .iter()
+            .map(|f| format!("( {f} )"))
+            .collect::<Vec<_>>()
+            .join(" & "),
+    );
     final_line.push_str(";\n");
     prism_input.push_str(&final_line);
-
 
     // define the global system as the product of all n many copies.
     // This uses prisms syntax for parallel composition.
     prism_input.push_str("\nsystem\n");
     let prod_string = (1..=n)
-                    .map(|i| format!("M{i}"))
-                    .collect::<Vec<String>>()
-                    .join(" || ");
+        .map(|i| format!("M{i}"))
+        .collect::<Vec<String>>()
+        .join(" || ");
     prism_input.push_str(&prod_string);
     prism_input.push_str("\nendsystem\n");
 
